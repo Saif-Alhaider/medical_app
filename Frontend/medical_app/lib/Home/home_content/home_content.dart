cea6f7b9f59ec54f006769f840fea2e5c654b66b
Saif Alhaider <saifharith90@gmail.com>
2022-09-15 20:44:35 +0300

    finished doctors section in home

--- Frontend/medical_app/lib/Home/home_content/home_content.dart
+++ Frontend/medical_app/lib/Home/home_content/home_content.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:medical_app/Home/constants.dart';
 
 import 'package:medical_app/Home/home_content/specialities.dart';
 import 'package:medical_app/reuseable_widgets/texts_types/headline_text.dart';
 import 'package:medical_app/reuseable_widgets/texts_types/sub_text.dart';
-
+import 'package:medical_app/models/doctors_images.dart';
 class HomeContent extends StatelessWidget {
   const HomeContent({super.key});
-  static final List<String> doctorsImages = [
-    'doctor-1.png',
-    'doctor-2.png',
-    'doctor-3.png',
-    'doctor-4.png',
-    'doctor-5.png',
-  ];
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -22,30 +17,62 @@ class HomeContent extends StatelessWidget {
           const Specialities(),
           Column(
             children: [
+              ConstantValues.cardsGap,
+              ConstantValues.cardsGap,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  HeadLineText(text: "الاطباء",lineHeight: 1,),
+                  SubText(text: "المزيد")
+                ],
+              ),
+              ConstantValues.cardsGap,
               Container(
-                    
-                decoration: BoxDecoration(boxShadow: ConstantValues.cardShadow,),
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 60,
+                      spreadRadius: 0.5,
+                      color: Color.fromARGB(23, 0, 0, 0)
+                    )
+                  ],
+                ),
                 height: 350,
-                child: ListView.builder(scrollDirection: Axis.horizontal,itemCount: 10,itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.all( 10),
-                    width: 250,
-                    decoration: BoxDecoration(
-                    color: Color(0xffFDFDFD),
-                      borderRadius: BorderRadius.circular(12)
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Image.asset("Assets/images/doctor01.jpeg",fit: BoxFit.cover,),
-                          Text("د.محمد الاراكي")
-                        ],
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.all(10),
+                      width: 250,
+                      decoration: BoxDecoration(
+                          color: Color(0xffFDFDFD),
+                          borderRadius: BorderRadius.circular(12)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                                height: 200,
+                                child: Image.asset(
+                                  doctorsImages[index],
+                                  fit: BoxFit.cover,
+                                )),
+                            SubText(
+                              text: "د.محمد الاراكي",
+                              color: Colors.black,
+                            ),
+                            SubText(
+                              text: "اخصائي جراحة قلب",
+                              size: 15,
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  );
-                },),
+                    );
+                  },
+                ),
               )
             ],
           )
---
+++ Frontend/medical_app/lib/models/doctors_images.dart
@@ -0,0 +1,12 @@
+final List doctorsImages = [
+  "Assets/images/doctor-1.png",
+  "Assets/images/doctor-2.png",
+  "Assets/images/doctor-3.png",
+  "Assets/images/doctor-4.png",
+  "Assets/images/doctor-5.png",
+  "Assets/images/doctor01.jpeg",
+  "Assets/images/doctor02.png",
+  "Assets/images/doctor03.jpeg",
+  "Assets/images/doctor04.jpeg",
+  "Assets/images/doctor05.jpeg",
+];
