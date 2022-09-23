7d86c73e94d873bd1d340e7718f9ff0b5fc4ce65
Saif Alhaider <saifharith90@gmail.com>
2022-09-23 03:30:01 +0300

    fixed welcome section infinity overflow

--- Frontend/medical_app/lib/Home/welcome_section.dart
+++ Frontend/medical_app/lib/Home/welcome_section.dart
@@ -21,37 +21,37 @@ class _WelcomeSectionState extends State<WelcomeSection> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              HeadLineText(
-                text: "مرحبا",
-                lineHeight: 1,
-                size: 28,
-                color: Colors.black,
-                fontWeight: FontWeight.w300,
-              ),
-              HeadLineText(
-                text: 'ابحث عن طبيب او عيادة',
-                lineHeight: 2,
-                size: 24,
-                color: Colors.black,
-                fontWeight: FontWeight.w800,
-              ),
-            ],
-          ),
-          const CircleAvatar(
-            backgroundImage:
-                NetworkImage('https://api.lorem.space/image/face?w=150&h=150'),
-            radius: 40,
-          )
-        ],
-      ),
-    );
+    return SizedBox(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                HeadLineText(
+                  text: "مرحبا",
+                  lineHeight: 1,
+                  size: 28,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w300,
+                ),
+                HeadLineText(
+                  text: 'ابحث عن طبيب او عيادة',
+                  lineHeight: 2,
+                  size: 24,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w800,
+                ),
+              ],
+            ),
+            const CircleAvatar(
+              backgroundImage:
+                  NetworkImage('https://api.lorem.space/image/face?w=150&h=150'),
+              radius: 40,
+            )
+          ],
+        ),
+      );
   }
 }
