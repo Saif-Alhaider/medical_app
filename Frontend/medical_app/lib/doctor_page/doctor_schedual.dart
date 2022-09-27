fe2db8df32dba56e7ad704ee20fdfcc5155c3b26
Saif Alhaider <saifharith90@gmail.com>
2022-09-28 02:00:53 +0300

    when select time appear main button

--- Frontend/medical_app/lib/doctor_page/doctor_schedual.dart
+++ Frontend/medical_app/lib/doctor_page/doctor_schedual.dart
@@ -21,99 +21,110 @@ class DoctorSchedual extends StatelessWidget {
   int? selectedIndex;
   @override
   Widget build(BuildContext context) {
+    DateTime? sentDate;
     Map<DateTime, List<TimeOfDay>> active_dates2 =
         convert_to_active_dates(activeDates: active_dates);
-    DateTime selectedDate = active_dates2.keys.first;
+    // DateTime selectedDate = active_dates2.keys.first;
+    DateTime? selectedDate;
     return StatefulBuilder(
       builder: (BuildContext context, setState) {
         return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const HeadLineText(
-          text: 'جدول المواعيد',
-          size: 24,
-          color: Colors.black,
-        ),
-        DatePicker(
-          daysCount: 30,
-          width: 80,
-          height: 110,
-          DateTime.now(),
-          initialSelectedDate: selectedDate,
-          selectionColor: Colors.black,
-          selectedTextColor: Colors.white,
-          activeDates: active_dates2.keys.toList(),
-          dateTextStyle: GoogleFonts.vazirmatn(fontSize: 18),
-          monthTextStyle: GoogleFonts.vazirmatn(fontSize: 18),
-          dayTextStyle: GoogleFonts.vazirmatn(fontSize: 18),
-          deactivatedColor: Colors.grey,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const HeadLineText(
+              text: 'جدول المواعيد',
+              size: 24,
+              color: Colors.black,
+            ),
+            DatePicker(
+              daysCount: 30,
+              width: 80,
+              height: 110,
+              DateTime.now(),
+              initialSelectedDate: null, //selectedDate
+              selectionColor: Colors.black,
+              selectedTextColor: Colors.white,
+              activeDates: active_dates2.keys.toList(),
+              dateTextStyle: GoogleFonts.vazirmatn(fontSize: 18),
+              monthTextStyle: GoogleFonts.vazirmatn(fontSize: 18),
+              dayTextStyle: GoogleFonts.vazirmatn(fontSize: 18),
+              deactivatedColor: Colors.grey,
 
-          // inactiveDates: [DateTime(2022,9,12)],
-
-          onDateChange: (date) {
-            setState(() {
-              selectedDate = date;
-              // print(selectedDate);
-            });
-          },
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child:
-              Breakline(color: Color.fromARGB(255, 208, 208, 208), height: 2),
-        ),
-        Center(
-          child: SizedBox(
-            height: 200,
-            child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    childAspectRatio: (1 / .5),
-                    crossAxisCount: 3,
-                  ),
-                  itemCount: active_dates2[selectedDate]!
-                      .length, //timeSchedual[selectedDate]!.length
-                  itemBuilder: (BuildContext context, int index) {
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          print(active_dates2[selectedDate]!.length);
-                          // time = active_dates2[selectedDate]![index];
-                          selectedIndex = index;
-                        });
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.all(4),
-                        decoration: BoxDecoration(
-                            color: selectedIndex == index
-                                ? Colors.black
-                                : Colors.grey,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Center(
-                          child: Directionality(
-                            textDirection: TextDirection.ltr,
-                            child: Text(
-                              active_dates2[selectedDate]![index]
-                                  .format(context),
-                              style: GoogleFonts.vazirmatn(
-                                  color: Colors.white, fontSize: 20),
-                            ),
-                          ),
+              onDateChange: (date) {
+                setState(() {
+                  selectedDate = date;
+                  // print(selectedDate);
+                });
+              },
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Breakline(
+                  color: Color.fromARGB(255, 208, 208, 208), height: 2),
+            ),
+            selectedDate != null
+                ? Center(
+                    child: SizedBox(
+                      height: 200,
+                      child: GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          childAspectRatio: (1 / .5),
+                          crossAxisCount: 3,
                         ),
+                        itemCount: active_dates2[selectedDate]!
+                            .length, //timeSchedual[selectedDate]!.length
+                        itemBuilder: (BuildContext context, int index) {
+                          return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                sentDate = DateTime(
+                                    selectedDate!.year,
+                                    selectedDate!.month,
+                                    selectedDate!.day,
+                                    active_dates2[selectedDate]![index].hour,
+                                    active_dates2[selectedDate]![index].minute);
+
+                                // time = active_dates2[selectedDate]![index];
+                                selectedIndex = index;
+                              });
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.all(4),
+                              decoration: BoxDecoration(
+                                  color: selectedIndex == index
+                                      ? Colors.black
+                                      : Colors.grey,
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Center(
+                                child: Directionality(
+                                  textDirection: TextDirection.ltr,
+                                  child: Text(
+                                    active_dates2[selectedDate]![index]
+                                        .format(context),
+                                    style: GoogleFonts.vazirmatn(
+                                        color: Colors.white, fontSize: 20),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
                       ),
-                    );
-                  },
-                ),
-          ),
-        ),
-        MainButton(
-            onPressed: () {
-              time == null
-                  ? print("can't make appointment")
-                  : Navigator.pop(context);
-            },
-            buttonTitle: "احجز")
-      ],
-    );
+                    ),
+                  )
+                : SizedBox(),
+            sentDate != null
+                ? MainButton(
+                    onPressed: () {
+                      time == null
+                          ? print("can't make appointment")
+                          : Navigator.pop(context);
+                    },
+                    buttonTitle: "احجز")
+                : SizedBox()
+          ],
+        );
       },
     );
   }
